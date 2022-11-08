de4c6967623ca4a39ddb8e4a2f04251b002004ef
Rahul Baliram Pawar <rahul>
2022-11-08 18:34:26 +0530

    commit in second branch

---
+++ src/main/java/com/gitdemo/Demo1.java
@@ -0,0 +1,14 @@
+package com.gitdemo;
+
+/**
+ * 
+ * @author Rahul Baliram Pawar
+ *
+ */
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("in the Demo1 class");
+	}
+}
